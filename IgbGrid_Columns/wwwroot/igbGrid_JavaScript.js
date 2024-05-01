32012010fb4b010012b19df81d61e76577cdf98d
mkamiishi <mkamiishi@infragistics.com>
2024-05-01 15:05:54 +0900

    Initial commit
    初回コミット

---
+++ IgbGrid_Columns/wwwroot/igbGrid_JavaScript.js
@@ -0,0 +1,5 @@
+﻿export const getCurrentColumns = (gridSelector) => {
+    const grid = document.querySelector(gridSelector);
+    if (!grid) return [];
+    return grid.columns.map(c => c["field"]);
+}
